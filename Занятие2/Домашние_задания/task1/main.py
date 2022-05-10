f72f9069a69b59751a526b0f369a34053ae58659
r-elen <elen.ryazanova@gmail.com>
2022-05-10 19:51:38 +0300

    homework2 is done 10.05

--- Занятие2/Домашние_задания/task1/main.py
+++ Занятие2/Домашние_задания/task1/main.py
@@ -1,14 +1,38 @@
+import time
+
+
+def work_time(fn):
+    def wrapper(*args, **kwargs):
+        t = time.time()
+        result = fn(*args, **kwargs)
+        dt = time.time() - t
+        print(f'Время работы {dt:.2f}')
+        return result
+    return wrapper
+
+
 def geom_progress(b, q):
     while True:
         yield b
         b = b * q
 
 
-if __name__ == "__main__":
-    my_count = geom_progress(1, 0.5)
-    for _ in range(5):
-        print(next(my_count))
+# def get_sum(iter_, desired_sum):
 
-    # sum_ = 0
-    # while sum_ < 1.8:
-    #     sum += next(my_count)
+
+@work_time
+def task():
+    my_count = geom_progress(1, 0.5)
+    # for _ in range(5):
+    #     print(next(my_count))
+    n = 0
+    sum_ = 0
+    while sum_ < 2.000001:
+        x = next(my_count)
+        sum_ += x
+        n += 1
+        print(f'{n}, {sum_:.10f}')
+
+
+if __name__ == "__main__":
+    task()
